27c38628860a9d21e37043e1e63e3521bf2ec33e
Armin <arminsh610@gmail.com>
2026-02-02 00:21:57 +0330

    Refactor: refactor the parser to reduce the allocation per request

--- internal/protocol/parser.go
+++ internal/protocol/parser.go
@@ -11,6 +11,18 @@ var (
 	ErrWrongArgCount  = errors.New("wrong number of arguments")
 )
 
+var (
+	cmdCREATE   = []byte("CREATE")
+	cmdDROP     = []byte("DROP")
+	cmdLIST     = []byte("LIST")
+	cmdDESCRIBE = []byte("DESCRIBE")
+	cmdSET      = []byte("SET")
+	cmdGET      = []byte("GET")
+	cmdDEL      = []byte("DEL")
+	cmdINCR     = []byte("INCR")
+	cmdSTATS    = []byte("STATS")
+)
+
 type Parser struct {
 	tokenizer *Tokenizer
 }
@@ -28,31 +40,31 @@ func (p *Parser) Parse(line []byte) (*Command, error) {
 	}
 
 	switch {
-	case bytes.EqualFold(tokens[0], []byte("CREATE")):
+	case bytes.EqualFold(tokens[0], cmdCREATE):
 		return parseCreate(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("DROP")):
+	case bytes.EqualFold(tokens[0], cmdDROP):
 		return parseDrop(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("LIST")):
+	case bytes.EqualFold(tokens[0], cmdLIST):
 		return parseList(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("DESCRIBE")):
+	case bytes.EqualFold(tokens[0], cmdDESCRIBE):
 		return parseDescribe(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("SET")):
+	case bytes.EqualFold(tokens[0], cmdSET):
 		return parseSet(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("GET")):
+	case bytes.EqualFold(tokens[0], cmdGET):
 		return parseGet(tokens)
 		//
-	case bytes.EqualFold(tokens[0], []byte("DEL")):
+	case bytes.EqualFold(tokens[0], cmdDEL):
 		return parseDel(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("INCR")):
+	case bytes.EqualFold(tokens[0], cmdINCR):
 		return parseIncr(tokens)
 
-	case bytes.EqualFold(tokens[0], []byte("STATS")):
+	case bytes.EqualFold(tokens[0], cmdSTATS):
 		return parseStats(tokens)
 	}
 
